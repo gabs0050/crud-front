42aea8159f129d8970e0501a8cfbfed6af1230e2
GABRIEL SOUZA COSTA <24122537@CFP127ALUNO.LOCAL>
2025-04-17 11:41:10 -0300

    Código atualizado. Post, Put e Delete feitos

--- contato.js
+++ contato.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// LISTA TODOS OS CONTATSOS
 async function getContatos () {
     const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
     const response = await fetch(url)
@@ -8,6 +9,7 @@ async function getContatos () {
     return data
 }
 
+// LISTA CONTATOS POR ID
 async function getContato (id) {
     const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
     const response = await fetch(url)
@@ -16,6 +18,7 @@ async function getContato (id) {
     return data
 }
 
+// FAZ O POST DE UM CONTATO
 async function postContato (contato) {
     const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
     const options = {
@@ -30,11 +33,37 @@ async function postContato (contato) {
     return response.ok
 }
 
+// ATUALIZA AS INFORMAÇÕES DE UM CONTATO
+async function putContato (contato, id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+// DELETA UM CONTATO POR ID
+async function deleteContato (id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'DELETE'
+    }
+    const response = await fetch(url, options)
+    return response.ok
+}
+
+// ARRAY PARA UTILIZAR COMO EXEMPLO
 const novoContato = {
     "nome": "Gabriel Souza Costa",
-    "celular": "11 9 9999-9999",
-    "foto": "gabrielzinho.png",
-    "email": "gabrielsouzacosta@gmail.com",
+    "celular": "11 8 8888-8888",
+    "foto": "gabriel.png",
+    "email": "souzacosta@gmail.com",
     "endereco": "Av. São Joaquim, 234",
-    "cidade": "Jandira"
+    "cidade": "Osasco"
 }
