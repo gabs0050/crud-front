7683a7e2908b53666a270d6eb4ea20ea5a020e5b
GABRIEL SOUZA COSTA <24122537@CFP127ALUNO.LOCAL>
2025-05-15 10:32:52 -0300

    Código atualizado

--- js/contato.js
+++ js/contato.js
@@ -1,28 +1,27 @@
 'use strict'
 
-
-
-export async function getContatos(){
+export async function getContatos () {
     const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
-    const response =  await fetch(url)
+    const response = await fetch(url)
     const data = await response.json()
     return data
 }
-export async function getContatosPorNome(nome){
+export async function getContatosPorNome (nome) {
     const url = `https://bakcend-fecaf-render.onrender.com/contatos?nome_like=^${nome}`
-    const response =  await fetch(url)
+    const response = await fetch(url)
     const data = await response.json()
-    return data
-}
-async function getContato(id){
-    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
-    const response =  await fetch(url)
-    const data = await response.json()
-    console.log(data)
     return data
 }
 
-async function postContato(contato){
+async function getContato (id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log (data)
+    return data
+}
+
+export async function postContato(contato) {
     const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
     const options = {
         method: 'POST',
@@ -31,11 +30,12 @@ async function postContato(contato){
         },
         body: JSON.stringify(contato)
     }
-    const response = await fetch(url, options)
+    const response = await fetch (url, options)
+
     return response.ok
 }
 
-  async function putContato(id,contato){
+async function putContato(id, contato) {
     const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
     const options = {
         method: 'PUT',
@@ -44,18 +44,17 @@ async function postContato(contato){
         },
         body: JSON.stringify(contato)
     }
-    const response = await fetch(url, options)
+    const response = await fetch (url, options)
+
     return response.ok
 }
 
-async function deleteContato(id){
+
+async function deleteContato(id) {
     const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
     const options = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+        method: 'DELETE'
+    }    
     const response = await fetch(url, options)
     return response.ok
 }
--- js/main.js
+++ js/main.js
@@ -1,33 +1,83 @@
 'use strict'
 
-import { getContatos, getContatosPorNome } from './contato.js'
+import { getContatos, getContatosPorNome, postContato } from "./contato.js";
+import { uploadImageToAzure } from "./uploadImageToAzure.js"; // Corrigido para importar do arquivo correto
 
-function criarCard(contato) {
+async function handleUploadImage() {
+    const uploadParams = {
+        file: document.getElementById('foto').files[0],
+        storageAccount: 'tutorialuploadgabriel',
+        sasToken: 'sp=racwl&st=2025-05-15T12:28:18Z&se=2025-05-15T20:28:18Z&sv=2024-11-04&sr=c&sig=FV%2Bs%2FkeD2g3CRuaoy%2FD7Q10Hq7ENNTgEh%2FkPOPJH0D0%3D',
+        containerName: 'fotos',
+    };
+
+    await uploadImageToAzure(uploadParams);
+}
+
+function criarCard (contato) {
     const container = document.getElementById('container')
-    const card = document.createElement('div')
+    const card = document.createElement('div')    
     card.classList.add('card-contato')
     card.innerHTML = `
-        <img src="${contato.foto}" alt="">
-        <h2>${contato.nome}</h2>
-        <p>${contato.celular}</p>
+            <img src="${contato.foto}" alt="">
+            <h2>${contato.nome}</h2>
+            <p>${contato.celular}</p>
     `
     container.appendChild(card)
 }
 
-async function exibirContatos() {
+async function exibirContatos () {
+    const container = document.getElementById('container')
     const contatos = await getContatos()
+    container.replaceChildren()
     contatos.forEach(criarCard)
-
 }
-async function exibirPesquisa(evento) {
-    if(evento.key == 'Enter'){
-       const contatos = await getContatosPorNome(evento.target.value)
-       container.replaceChildren()
-       contatos.forEach(criarCard)
+
+async function exibirPesquisa (evento) {
+    const container = document.getElementById('container')
+    if (evento.key == 'Enter') {
+        const contatos = await getContatosPorNome(evento.target.value)
+        container.replaceChildren()
+        contatos.forEach(criarCard)
     }
+    
+}
+
+function cadastroContato () {
+    document.querySelector('main').className = 'form-show'
+}
+
+function voltarHome () {
+    document.querySelector('main').className = 'card-show'
+}
+async function salvarContato () {
+    const contato = {
+            "nome": document.getElementById('nome').value,
+            "celular": document.getElementById('celular').value,
+            "foto": document.getElementById('foto').value,
+            "email": document.getElementById('email').value,
+            "endereco": document.getElementById('endereco').value,
+            "cidade": document.getElementById('cidade').value
+        }
+
+        if (await postContato(contato)){
+            await exibirContatos()
+            voltarHome()
+            alert ('Cadastro realizado com sucesso!!!')
+        }
+        
 }
 
 exibirContatos()
 
 document.getElementById('pesquisar')
-document.addEventListener('keydown', exibirPesquisa)
+        .addEventListener('keydown',exibirPesquisa)
+
+document.getElementById('novo-contato')
+        .addEventListener('click', cadastroContato)
+
+document.getElementById('cancelar')
+        .addEventListener('click', voltarHome)
+
+document.getElementById('salvar')
+        .addEventListener('click', salvarContato)
